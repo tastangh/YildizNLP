76b09d5d6d2eca102e0c4a838a6b52edd475e64c
tastangh <mehmettastan2014@gmail.com>
2024-10-27 01:09:27 +0300

    changes

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import os
+
+# Dosya yolunu belirle
+file_path = '../data/turkish_instructions.csv'
 
 # CSV dosyasını okumaya çalış
 try:
-    df = pd.read_csv('../data/turkish_instructions.csv')  # Dosya yolu
-    print("CSV dosyası başarıyla yüklendi.")
+    # Dosyanın mevcut olup olmadığını kontrol et
+    if not os.path.isfile(file_path):
+        print(f"Hata: Dosya bulunamadı. Aranan yol: {file_path}")
+    else:
+        df = pd.read_csv(file_path)  # Dosya yolu
+        print("CSV dosyası başarıyla yüklendi.")
 except FileNotFoundError:
     print("Hata: Dosya bulunamadı. Lütfen dosya yolunu kontrol edin.")
 except Exception as e:
